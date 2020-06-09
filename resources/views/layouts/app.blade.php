cae4b491e92e6b380ec9c28411351855f825e182
Tabares <ytabaressilverio86@gmail.com>
2020-06-09 03:37:25 +0200

    fix: menu collapse responsive

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -15,6 +15,10 @@
     <link rel="mask-icon" href="{{ asset('safari-pinned-tab.svg') }}" color="#5bbad5">
 
     <!-- Scripts -->
+    <script src="{{ asset('js/jquery.js') }}" ></script>
+    <script src="{{ asset('js/bootstrap.js') }}" ></script>
+
+
     <script src="{{ asset('js/app.js') }}" ></script>
 
     <!-- Fonts -->
@@ -24,7 +28,8 @@
     <!-- Styles -->
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
     <link href="{{ asset('css/bootstrap.css') }}"  rel="stylesheet">
-{{--<script src="{{asset('js/jquery-2.0.0.min.js')}}"></script>--}}
+
+    {{--<script src="{{asset('js/jquery-2.0.0.min.js')}}"></script>--}}
 
 <!-- Add custom CSS here -->
 
@@ -39,7 +44,7 @@
 </head>
 <body>
     <div id="app">
-        <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm" >
+        <nav class="navbar navbar-expand-md navbar-light  bg-white shadow-sm " role="navigation">
             <div class="container">
                 <a class="navbar-brand" href="{{ url('/') }}">
                     {{--{{ config('app.name', 'Laravel') }}--}}
@@ -47,23 +52,22 @@
 
 
                 </a>
-                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
-                    <span class="navbar-toggler-icon"></span>
+                <button class="navbar-toggler" type="button" data-toggle="collapse"  data-target="#navbarSupportedContent" >
+                   <span class="navbar-toggler-icon"></span>
+
                 </button>
 
-                <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                <div class="collapse navbar-collapse " id="navbarSupportedContent">
                     <!-- Left Side Of Navbar -->
-                    <ul class="navbar-nav mr-auto">
 
-                    </ul>
 
                     <!-- Right Side Of Navbar -->
-                    <ul class="navbar-nav ml-auto  navbar-right">
+                    <ul class="nav navbar-nav ml-auto  navbar-right" >
                         <!-- Authentication Links -->
                         @if(Auth::guard('web')->check())
                             <a href="#" class="nav-link " data-toggle="dropdown"><i class="icon-user"></i>    {{ Auth::user()->name }} <b class="caret"></b></a>
 
-                            <ul class="dropdown-menu">
+                            <ul class="dropdown-menu " >
 
                                     <li><a href="{{url('perfil')}}"><i class="icon-user"></i> Perfil</a></li>
 
@@ -85,8 +89,10 @@
                             @endif
                     </ul>
                 </div>
+
             </div>
         </nav>
+
 
         <main class="py-4" >
             @yield('content')
